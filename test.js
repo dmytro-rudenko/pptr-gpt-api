6f11991815afa8c540ad73484aaa1bc518c618e9
dmytro-rudenko <ph.dmitry.rudenko@gmail.com>
2024-04-09 00:51:46 +0300

    update

--- test.js
+++ test.js
@@ -15,13 +15,27 @@ const test = async () => {
 
     console.log('answer', answer)
 
+    const ask2 = await fetch(`http://${HOST}/ask`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            question: "Who is the creator of puppeteer?",
+        }),
+    })
+
+    const answer2 = await ask2.json()
+
+    console.log('answer2', answer2)
+
     const createChat = await fetch(`http://${HOST}/create-chat`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            message: "What is the meaning of life?",
+            message: "Create a story about dog, coder and nuclear bomb",
         }),
     })
 
@@ -36,13 +50,28 @@ const test = async () => {
         },
         body: JSON.stringify({
             id: chat.id,
-            message: "Translate answer to ukrainian",
+            message: "It must be in Philip Dick's style",
         }),
     })
 
     const message = await sendMessage.json()
 
     console.log('message', message)
+
+    const nextMessage = await fetch(`http://${HOST}/chat/send-message`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: chat.id,
+            message: "Translate it to ukrainian",
+        }),
+    })
+
+    const nextMessage2 = await nextMessage.json()
+
+    console.log('nextMessage2', nextMessage2)
 }
 
 test()
